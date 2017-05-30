13153754eee25cadadd301691a0e1b54501d148f
马远征 <myz1104@126.com>
2017-05-31 01:54:25 +0800

    更新设置页面

--- FitCloudDemo/FitCloudDemo/ViewControllers/设置/ViewController/FCFrimwareViewController.m
+++ FitCloudDemo/FitCloudDemo/ViewControllers/设置/ViewController/FCFrimwareViewController.m
@@ -14,24 +14,33 @@
 
 @implementation FCFrimwareViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+#pragma mark - dealloc
+
+- (void)dealloc
+{
+    
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - 按钮交互
+
+- (IBAction)clickToBack:(id)sender
+{
+    [self.navigationController popViewControllerAnimated:YES];
 }
-*/
+
+
+#pragma mark - lifeStyle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
 
 @end
--- FitCloudDemo/FitCloudDemo/ViewControllers/设置/ViewController/FCOtherViewController.m
+++ FitCloudDemo/FitCloudDemo/ViewControllers/设置/ViewController/FCOtherViewController.m
@@ -14,24 +14,29 @@
 
 @implementation FCOtherViewController
 
+#pragma mark - dealloc
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+
+#pragma mark - 按钮交互
+
+- (IBAction)clickToBack:(id)sender
+{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+
+#pragma mark - lifeStyle
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
