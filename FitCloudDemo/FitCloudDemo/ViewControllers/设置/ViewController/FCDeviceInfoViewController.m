f7359aec5bdd8aa8ae9172a28d13455904cb7b45
马远征 <myz1104@126.com>
2017-06-05 15:10:35 +0800

    手表设备信息更新

--- FitCloudDemo/FitCloudDemo/ViewControllers/设置/ViewController/FCDeviceInfoViewController.m
+++ FitCloudDemo/FitCloudDemo/ViewControllers/设置/ViewController/FCDeviceInfoViewController.m
@@ -8,8 +8,10 @@
 
 #import "FCDeviceInfoViewController.h"
 
-@interface FCDeviceInfoViewController ()
-
+@interface FCDeviceInfoViewController () <UITableViewDataSource,UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UILabel *batteryLevel;
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+@property (nonatomic, weak) IBOutlet UITableView *tableView;
 @end
 
 @implementation FCDeviceInfoViewController
@@ -38,5 +40,87 @@
     [super viewDidLoad];
 }
 
+#pragma mark - UITableView
 
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 8;
+}
+
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 50.0;
+}
+
+
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row == 0) {
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"NameCell" forIndexPath:indexPath];
+        
+        cell.textLabel.text = @"设备名称";
+        return cell;
+    }
+    else if (indexPath.row == 1)
+    {
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"FindWatchCell" forIndexPath:indexPath];
+        cell.textLabel.text = @"查找手环";
+        return cell;
+    }
+    else if (indexPath.row == 2)
+    {
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"DisplayCell" forIndexPath:indexPath];
+        cell.textLabel.text = @"手环显示设置";
+        cell.detailTextLabel.text = @"设置是否在手环上显示";
+        return cell;
+    }
+    else if (indexPath.row == 3)
+    {
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"WearStyleCell" forIndexPath:indexPath];
+        cell.textLabel.text = @"佩戴方式";
+        return cell;
+    }
+    else if (indexPath.row == 4 || indexPath.row == 5)
+    {
+        if (indexPath.row == 4) {
+            UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SwitchCell" forIndexPath:indexPath];
+            cell.textLabel.text = @"翻腕亮屏";
+            cell.detailTextLabel.text = @"翻手腕查看手环信息";
+            return cell;
+        }
+        else
+        {
+            UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SwitchCell" forIndexPath:indexPath];
+            cell.textLabel.text = @"健康定时监测";
+            cell.detailTextLabel.text = @"开启时定时自动检测，生成历史记录";
+            return cell;
+        }
+    }
+    else
+    {
+         if (indexPath.row == 6)
+         {
+             UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TimeCell" forIndexPath:indexPath];
+             cell.textLabel.text = @"开始时间";
+             return cell;
+         }
+        else
+        {
+            UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TimeCell" forIndexPath:indexPath];
+            cell.textLabel.text = @"结束时间";
+            return cell;
+        }
+    }
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
 @end
